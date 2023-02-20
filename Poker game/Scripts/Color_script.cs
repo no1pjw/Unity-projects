f3e380158ee770b3f9497db7197c4cedbcec475d
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2023-02-20 15:56:17 +0900

    Update Color_script.cs

--- Poker game/Scripts/Color_script.cs
+++ Poker game/Scripts/Color_script.cs
@@ -6,6 +6,7 @@ public class Color_script : MonoBehaviour
 {
     public GameObject player_state_text;
     public GameObject ai_state_text;
+    public GameObject win_text;
     // Start is called before the first frame update
     void Start()
         
@@ -60,6 +61,24 @@ public class Color_script : MonoBehaviour
             StartCoroutine("Player_FadeIn");
         }
     }
+    public void Yellow(GameObject obj, int player)
+    {
+        Text text = obj.GetComponent<Text>();
+        text.color = new Color(255, 255, 0, 0);
+        if (player == 0)
+        {
+            text.text = "AI win!";
+        }
+        else if(player == 1)
+        {
+            text.text = "Player win!";
+        }
+        else
+        {
+            text.text = "Draw!";
+        }
+        StartCoroutine("Win_FadeIn");
+    }
     IEnumerator Player_FadeIn()
     {
         Text text = player_state_text.GetComponent<Text>();
@@ -76,6 +95,19 @@ public class Color_script : MonoBehaviour
     IEnumerator AI_FadeIn()
     {
         Text text = ai_state_text.GetComponent<Text>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            float f = i / 10.0f;
+            Color c = text.color;
+            c.a = f;
+            text.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    IEnumerator Win_FadeIn()
+    {
+        Text text = win_text.GetComponent<Text>();
 
         for (int i = 0; i < 10; i++)
         {
